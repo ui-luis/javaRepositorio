fe85a5798907a33feb5342d9b09e16fd48510341
Jose Luis <luis.js.py@gmail.com>
2023-09-08 14:45:38 -0400

    La rama de juan esta culminada con las especificaciones de vehuculo

--- src/programandoJavaPOO/Primario.java
+++ src/programandoJavaPOO/Primario.java
@@ -9,5 +9,18 @@ public class Primario {
         int age = obj.nextInt();
         int roomNumber = obj.nextInt();
 
+        Secundario secundario = new Secundario(0.1f,200);
+        System.out.println("La velocidad Inicial es :"+secundario.velocidadInicial);
+        secundario.acelerar();
+        System.out.println("Se acelero hasta :"+secundario.velocidadInicial);
+        System.out.println("La velocidad Maxima que llega es : "+secundario.velocidadMaxima);
+        secundario.setColor("Green");
+        String color = secundario.getColor();
+        System.out.println("El color de Vehiculo es : "+color);
+        secundario.setMarca("Suzuki");
+        System.out.println("La Marca del Vehiculo es :"+secundario.getMarca());
+
+
+
     }
 }
--- src/programandoJavaPOO/Secundario.java
+++ src/programandoJavaPOO/Secundario.java
@@ -1,4 +1,37 @@
 package programandoJavaPOO;
 
+import javax.sound.midi.Soundbank;
+
 public class Secundario {
+    // atributos
+    // constructores
+    // metodos
+   private String color;
+   private String marca;
+
+   public String getMarca(){
+       return marca;
+   }
+   public void setMarca(String marca){
+       this.marca = marca;
+   }
+   public void setColor(String color){
+       this.color = color;
+   }
+   public String getColor(){
+       return color;
+   }
+    float velocidadInicial;
+    float velocidadMaxima;
+
+    public Secundario(float velocidadInicial,float velocidadMaxima){
+        System.out.println("Estoy en el contructor ");
+        this.velocidadInicial = velocidadInicial;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public void acelerar(){
+        velocidadInicial +=15.2;
+    }
+
 }
