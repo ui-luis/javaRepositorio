bf0cb1fefdf8f29d47f2a5a38ec023409f75184f
Jose Luis <luis.js.py@gmail.com>
2023-09-08 14:17:05 -0400

    Esta es la version principal de la practica

---
+++ src/programandoJavaPOO/Primario.java
@@ -0,0 +1,13 @@
+package programandoJavaPOO;
+
+import java.util.Scanner;
+
+public class Primario {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String name = obj.nextLine();
+        int age = obj.nextInt();
+        int roomNumber = obj.nextInt();
+
+    }
+}
---
+++ src/programandoJavaPOO/Secundario.java
@@ -0,0 +1,4 @@
+package programandoJavaPOO;
+
+public class Secundario {
+}
