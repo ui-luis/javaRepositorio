0455b59e0c8dc99e81e9734cfa2698344321a59a
Jose Luis <luis.js.py@gmail.com>
2023-09-13 18:49:09 -0400

    lolo

--- src/programandoJavaPOO/Cuarta.java
+++ src/programandoJavaPOO/Cuarta.java
@@ -1,9 +1,13 @@
 package programandoJavaPOO;
 
+
 public class Cuarta {
    
         System.out.println("Segundo comentario ");
    
         System.out.println(" Hola saludos soy nuevo archivo");
+
+        System.out.println("Hola saludos soy nuevo archivo ");
+
 
 }
