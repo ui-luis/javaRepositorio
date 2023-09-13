4c10f63ec97ab7fe68dafe5bdc7a6dc556613b86
ui-luis <116533778+ui-luis@users.noreply.github.com>
2023-09-13 19:05:46 -0400

    Update Cuarto.java

--- src/programandoJavaPOO/Cuarto.java
+++ src/programandoJavaPOO/Cuarto.java
@@ -1,10 +1,8 @@
 package programandoJavaPOO;
 
-import java.util.Scanner;
 
 public class Cuarto {
 
-        System.out.println("El nombre es");
-        System.out.println("La edad es  Años");
+     int numero;
 
 }
